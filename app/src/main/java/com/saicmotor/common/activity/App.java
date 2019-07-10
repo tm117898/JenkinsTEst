d6225997022396d8ec8d9b8cf2cb341730ad3637
shanghaolongteng <shanghaolongteng@vargo.com.cn>
2019-07-10 11:12:56 +0800

    init add

---
+++ app/src/main/java/com/saicmotor/common/activity/App.java
@@ -0,0 +1,16 @@
+package com.saicmotor.common.activity;
+
+import android.app.Application;
+
+/**
+ * <br/>
+ * <li>Author Cesc Shang
+ * <li>Email v_shanghaolongteng@saicmotor.com
+ * <li>Date 2019/6/17 16:20
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
